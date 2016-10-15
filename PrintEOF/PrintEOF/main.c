ecea0e10ee3c449f7fd19b8b31ac93f32952dff9
Jason <c3171700@uon.edu.au>
2016-10-15 15:05:35 +0900

    1-7_DefinedTo-1

--- PrintEOF/PrintEOF/main.c
+++ PrintEOF/PrintEOF/main.c
@@ -9,19 +9,17 @@
 
 #include <stdio.h>
 
-// Before I start it, I reckon that printing out of EOF value is not even making sense AT ALL as it's defined as -1.
+// Excercise 1-6 & 1-7
 int main(int argc, const char * argv[]) {
+    char c;
     
-    // Excercise 1-6 & 1-7
-    int c;
-
+    // Excercise 1-7 : Value of EOF is defined as -1
+    printf("Value of EOF is defined as : %d\n", EOF);
+    
     while ((c=getchar()) != EOF){
         // Excercise 1-6 : if 'c=getchar()!=EOF' is 0, the while loop would not be executed,
         //                  so the value must be '1'
         putchar(c);
-        
-        // Excercise 1-7 : Print out the value of EOF
-        printf("EOF is : %d\n", EOF);
     }
     
     return 0;
